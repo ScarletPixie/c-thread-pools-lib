b91f7fc4994be1e7733157453a719e000cb7d9d0
cranberry <scarletskynned@gmail.com>
2026-08-26 15:47:41 -0300

    refactor: organize and remove unecessary code

--- ctp.c
+++ ctp.c
@@ -8,33 +8,77 @@
 #include <stdio.h>
 
 
-typedef struct _task_queue _task_queue_t;
-static _task_queue_t* _task_queue_create(size_t size);
-static void _task_queue_destroy(_task_queue_t *queue);
-static int _task_queue_push(_task_queue_t *queue, ctp_task_t *task);
-static ctp_task_t* _task_queue_pop(_task_queue_t *queue);
-static bool _task_queue_is_empty(_task_queue_t *queue);
-static bool _task_queue_full(_task_queue_t *queue);
-static ctp_task_t* _task_queue_search(_task_queue_t *queue, ctp_task_t* task);
-static size_t _task_queue_remaining_slots(_task_queue_t *queue);
-
+/// @name Worker Thread - Summary
+/// Functions for initializing thread workers and the worker routine.
+/// @{
 
 typedef struct
 {
     pthread_t thread;
-    ctp_task_t *task;
     ctp_pool_t *pool;
 } _worker;
 
+/// @brief Initializes the `_worker` object.
+/// @param worker A pointer to a worker object.
+/// @param pool The pool the worker belongs to. Must not be `NULL`.
+/// @return Returns `worker` on success, or `NULL` on failure.
+static _worker* _worker_init(_worker* worker, ctp_pool_t* pool);
+
+/// @brief The routine of each worker thread in the thread pool.
+/// @param worker A pointer to a `_worker` object. Must not be of any other type.
+/// @return Always returns NULL.
+static void* _worker_routine(void* worker);
+
+/// @}
+
+
+/// @name Task Queue - Summary
+/// Functions for initializing and managing the task queue.
+/// @{
+
 typedef struct _task_queue
 {
-    ctp_task_t **tasks;
     size_t size;
     size_t current;
-
-    bool has_work;
+    ctp_task_t* tasks[];
 } _task_queue_t;
 
+/// @brief Creates a new task queue object.
+/// @param size The fixed number of slots available in the queue.
+/// @return Returns a New task queue object.
+static _task_queue_t*   _task_queue_create(size_t size);
+
+/// @brief Destroys a task queue object and every task inside it.
+static void             _task_queue_destroy(_task_queue_t *queue);
+
+/// @brief Inserts a new task into the queue.
+/// @return Returns 0 on success, -1 on failure like if the queue is full for example.
+static int              _task_queue_push(_task_queue_t *queue, ctp_task_t *task);
+
+/// @brief Removes the next task present inside the queue.
+/// @return Returns the removed task or NULL if there are no elements to be removed.
+static ctp_task_t*      _task_queue_pop(_task_queue_t *queue);
+
+/// @brief Returns whether the queue is full of tasks.
+static bool             _task_queue_full(_task_queue_t *queue);
+
+/// @brief Searches the queue for the input task.
+/// @param task The task to be searched inside the queue.
+/// @return Returns `task` if it's found, `NULL` otherwise.
+static ctp_task_t*      _task_queue_search(_task_queue_t *queue, ctp_task_t* task);
+
+/// @brief Returns whether there are no tasks stored in the queue.
+static bool             _task_queue_is_empty(_task_queue_t *queue);
+
+/// @brief Gets the remaining number of free task slots inside the queue.
+static size_t           _task_queue_remaining_slots(_task_queue_t *queue);
+
+/// @}
+
+
+/// @name Task Management - Summary
+/// Functions for creating and destroying tasks.
+/// @{
 
 struct ctp_task
 {
@@ -42,12 +86,24 @@ struct ctp_task
     void *arg;
 
     pthread_mutex_t mutex;
-    pthread_cond_t task_completed_cond;
+    pthread_cond_t cond;
 
     atomic_uint remaining;
     atomic_uint ref_count;
 };
-static void _ctp_task_release(ctp_pool_t* pool, ctp_task_t *task);
+
+ctp_task_t*     ctp_task_create(void (*function)(void *), void* arg);
+void            ctp_task_destroy(ctp_task_t* task);
+
+/// @brief Releases a reference to a task destroying it when the reference count reaches 0.
+static void     _ctp_task_release(ctp_pool_t* pool, ctp_task_t *task);
+
+/// @}
+
+
+/// @name Thread Pool - Summary
+/// Functions for creating and destroying thread pools.
+/// @{
 
 typedef struct ctp_pool
 {
@@ -59,51 +115,21 @@ typedef struct ctp_pool
 
     bool stopping;
     pthread_mutex_t mutex;
-    pthread_cond_t task_cond;
+    pthread_cond_t cond;
 
 } ctp_pool_t;
 
+int ctp_submit_task(ctp_pool_t* pool, ctp_task_t* task);
+int ctp_submit_task_n(ctp_pool_t* pool, ctp_task_t* task, size_t n);
+void ctp_wait_task(ctp_pool_t* pool, ctp_task_t* task);
+void ctp_wait_all(ctp_pool_t* pool);
 
-static void* _routine(void* arg)
-{
-    _worker* const worker = (_worker *)arg;
+/// @}
 
-    while (true)
-    {
-        pthread_mutex_lock(&worker->pool->mutex);
-        while (_task_queue_is_empty(worker->pool->queue) && !worker->pool->stopping)
-            pthread_cond_wait(&worker->pool->task_cond, &worker->pool->mutex);
 
-        if (worker->pool->stopping && _task_queue_is_empty(worker->pool->queue))
-        {
-            pthread_mutex_unlock(&worker->pool->mutex);
-            break;
-        }
-
-        if (_task_queue_full(worker->pool->active))
-        {
-            pthread_mutex_unlock(&worker->pool->mutex);
-            continue;
-        }
-        
-        worker->task = _task_queue_pop(worker->pool->queue);
-        assert(worker->task != NULL);
-        _task_queue_push(worker->pool->active, worker->task);
-        pthread_mutex_unlock(&worker->pool->mutex);
-
-        worker->task->function(worker->task->arg);
-
-        pthread_mutex_lock(&worker->task->mutex);
-        if (atomic_fetch_sub(&worker->task->remaining, 1) == 1)
-            pthread_cond_signal(&worker->task->task_completed_cond);
-        pthread_mutex_unlock(&worker->task->mutex);
-        _ctp_task_release(worker->pool, worker->task);
-        worker->task = NULL;
-    }
-
-    return NULL;
-}
-
+/// @name Thread Pool - Implementation
+/// Functions for creating and destroying thread pools.
+/// @{
 
 ctp_pool_t *ctp_pool_create(size_t num_workers, size_t queue_size)
 {
@@ -112,7 +138,7 @@ ctp_pool_t *ctp_pool_create(size_t num_workers, size_t queue_size)
         return NULL;
 
     pthread_mutex_init(&pool->mutex, NULL);
-    pthread_cond_init(&pool->task_cond, NULL);
+    pthread_cond_init(&pool->cond, NULL);
 
     pool->num_workers = num_workers;
     pool->stopping = false;
@@ -137,14 +163,12 @@ ctp_pool_t *ctp_pool_create(size_t num_workers, size_t queue_size)
 
     for (size_t i = 0; i < num_workers; ++i)
     {
-        pool->workers[i].pool = pool;
-        pool->workers[i].task = NULL;
-
-        if (pthread_create(&pool->workers[i].thread, NULL, _routine, &pool->workers[i]) != 0)
+        if (!_worker_init(pool->workers + i, pool))
         {
             for (size_t j = 0; j < i; ++j)
                 pthread_join(pool->workers[j].thread, NULL);
             _task_queue_destroy(pool->queue);
+            _task_queue_destroy(pool->active);
             free(pool->workers);
             free(pool);
             return NULL;
@@ -167,7 +191,7 @@ int ctp_submit_task(ctp_pool_t *pool, ctp_task_t* task)
         return -1;
     }
     atomic_fetch_add(&task->remaining, 1);
-    pthread_cond_signal(&pool->task_cond);
+    pthread_cond_signal(&pool->cond);
     pthread_mutex_unlock(&pool->mutex);
 
     return 0;
@@ -190,7 +214,7 @@ int ctp_submit_task_n(ctp_pool_t* pool, ctp_task_t* task, size_t n)
         }
         atomic_fetch_add(&task->remaining, 1);
     }
-    pthread_cond_broadcast(&pool->task_cond);
+    pthread_cond_broadcast(&pool->cond);
     pthread_mutex_unlock(&pool->mutex);
     return 0;
 }
@@ -211,7 +235,7 @@ void ctp_wait_task(ctp_pool_t *pool, ctp_task_t *task)
 
     pthread_mutex_lock(&task->mutex);
     while (task->remaining != 0)
-        pthread_cond_wait(&task->task_completed_cond, &task->mutex);
+        pthread_cond_wait(&task->cond, &task->mutex);
     pthread_mutex_unlock(&task->mutex);
     _ctp_task_release(pool, task);
 }
@@ -248,7 +272,7 @@ void ctp_pool_destroy(ctp_pool_t *pool)
 
     pthread_mutex_lock(&pool->mutex);
     pool->stopping = true;
-    pthread_cond_broadcast(&pool->task_cond);
+    pthread_cond_broadcast(&pool->cond);
     pthread_mutex_unlock(&pool->mutex);
     for (size_t i = 0; i < pool->num_workers; ++i)
         pthread_join(pool->workers[i].thread, NULL);
@@ -258,6 +282,13 @@ void ctp_pool_destroy(ctp_pool_t *pool)
     free(pool->workers);
     free(pool);
 }
+
+/// @}
+
+
+/// @name Task Management - Implementation
+/// Functions for creating and destroying tasks.
+/// @{
 
 ctp_task_t *ctp_task_create(void (*function)(void *), void *arg)
 {
@@ -272,7 +303,7 @@ ctp_task_t *ctp_task_create(void (*function)(void *), void *arg)
     task->remaining = 0;
 
     pthread_mutex_init(&task->mutex, NULL);
-    pthread_cond_init(&task->task_completed_cond, NULL);
+    pthread_cond_init(&task->cond, NULL);
 
     task->ref_count = 1;
 
@@ -303,10 +334,26 @@ void ctp_task_destroy(ctp_task_t *task)
         return;
 
     pthread_mutex_destroy(&task->mutex);
-    pthread_cond_destroy(&task->task_completed_cond);
+    pthread_cond_destroy(&task->cond);
     free(task);
 }
 
+/// @}
+
+
+/// @name Task Queue - Implementation
+/// Functions for initializing and managing the task queue.
+/// @{
+
+static _task_queue_t* _task_queue_create(size_t size)
+{
+    _task_queue_t *queue = calloc(1, sizeof(*queue) + size * sizeof(ctp_task_t));
+    if (!queue)
+        return NULL;
+
+    queue->size = size;
+    return queue;
+}
 
 static int _task_queue_push(_task_queue_t *queue, ctp_task_t *task)
 {
@@ -316,7 +363,6 @@ static int _task_queue_push(_task_queue_t *queue, ctp_task_t *task)
         return -1; // Queue is full
 
     queue->tasks[queue->current++] = task;
-    queue->has_work = true;
 
     atomic_fetch_add(&task->ref_count, 1);
 
@@ -327,37 +373,13 @@ static ctp_task_t* _task_queue_pop(_task_queue_t *queue)
 {
     assert(queue != NULL);
 
-    if (queue->current == 0 && !queue->has_work)
+    if (queue->current == 0)
         return NULL;
 
     ctp_task_t *current_task = queue->tasks[--queue->current];
-
-    if (queue->current == 0)
-        queue->has_work = false;
-
     queue->tasks[queue->current] = NULL;
 
     return current_task;
-}
-
-static _task_queue_t* _task_queue_create(size_t size)
-{
-    _task_queue_t *queue = malloc(sizeof(*queue));
-    if (!queue)
-        return NULL;
-
-    queue->tasks = calloc(size, sizeof(ctp_task_t *));
-    if (!queue->tasks)
-    {
-        free(queue);
-        return NULL;
-    }
-
-    queue->size = size;
-    queue->current = 0;
-    queue->has_work = false;
-
-    return queue;
 }
 
 static void _task_queue_destroy(_task_queue_t *queue)
@@ -368,16 +390,13 @@ static void _task_queue_destroy(_task_queue_t *queue)
     for (size_t i = 0; i < queue->current; ++i)
         ctp_task_destroy(queue->tasks[i]);
 
-    free(queue->tasks);
     free(queue);
 }
 static bool _task_queue_is_empty(_task_queue_t *queue)
 {
     assert(queue != NULL);
 
-    bool is_empty = (queue->current == 0 && !queue->has_work);
-
-    return is_empty;
+    return queue->current == 0;
 }
 
 static bool _task_queue_full(_task_queue_t *queue)
@@ -397,3 +416,65 @@ static size_t _task_queue_remaining_slots(_task_queue_t *queue)
 {
     return queue->size - queue->current;
 }
+
+/// @}
+
+
+/// @name Worker Thread - Implementation
+/// Functions for initializing thread workers and the worker routine.
+/// @{
+
+static _worker* _worker_init(_worker* worker, ctp_pool_t* pool)
+{
+    assert(pool);
+
+    worker->pool = pool;
+
+    if (pthread_create(&worker->thread, NULL, _worker_routine, worker) != 0)
+        return NULL;
+
+    return worker;
+}
+
+static void* _worker_routine(void* arg)
+{
+    _worker* const worker = (_worker *)arg;
+    ctp_pool_t* const pool = worker->pool;
+
+    while (true)
+    {
+        pthread_mutex_lock(&pool->mutex);
+        while (_task_queue_is_empty(pool->queue) && !pool->stopping)
+            pthread_cond_wait(&pool->cond, &pool->mutex);
+
+        if (pool->stopping && _task_queue_is_empty(pool->queue))
+        {
+            pthread_mutex_unlock(&pool->mutex);
+            break;
+        }
+
+        if (_task_queue_full(pool->active))
+        {
+            pthread_mutex_unlock(&pool->mutex);
+            continue;
+        }
+        
+        ctp_task_t* task = _task_queue_pop(pool->queue);
+        assert(task != NULL);
+        _task_queue_push(pool->active, task);
+        pthread_mutex_unlock(&pool->mutex);
+
+        task->function(task->arg);
+
+        pthread_mutex_lock(&task->mutex);
+        if (atomic_fetch_sub(&task->remaining, 1) == 1)
+            pthread_cond_signal(&task->cond);
+        pthread_mutex_unlock(&task->mutex);
+
+        _ctp_task_release(pool, task);
+    }
+
+    return NULL;
+}
+
+/// @}
