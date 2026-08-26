ccb14ae55f7501d249ba1ff3f4bf5775e8849953
cranberry <scarletskynned@gmail.com>
2026-08-26 15:59:39 -0300

    test(WIP): update test

--- tests/main.c
+++ tests/main.c
@@ -83,18 +83,15 @@ TEST test_ctp_pool_concurency(void)
     const size_t expected_ns = batches * sleep_us * 1000;
     const size_t slack_ns = expected_ns * 1.20;
 
-    const uint64_t start = monotonic_ns();
     ctp_task_t* task = ctp_task_create(sleep_task, (size_t*)&sleep_us);
     ASSERT_EQ(0, ctp_submit_task_n(pool, task, task_count));
-    const uint64_t end = monotonic_ns();
 
-
+    const uint64_t start = monotonic_ns();
     ctp_wait_all(pool);
+    const uint64_t end = monotonic_ns();
     const uint64_t elapsed = end - start;
 
-    printf("elapsed: %zu, maximum: %zu", elapsed, slack_ns);
-
-    //ASSERT_LTE(elapsed, slack_ns);
+    ASSERT_LTE(elapsed, slack_ns);
 
     ctp_pool_destroy(pool);
     PASS();
